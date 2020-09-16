a9dd94cf85a09bc75253c46ee439552f39b423f4
Nathan Gearke <n.gearke@gmail.com>
2020-09-15 23:38:27 -0500

    got the first part started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrbitListComponent } from './orbit-list/orbit-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrbitListComponent
   ],
   imports: [
     BrowserModule
